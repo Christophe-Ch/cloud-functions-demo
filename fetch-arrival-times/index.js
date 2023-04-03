ae24062978fe76f9844d38752f7c212545649ba1
Christophe CHICHMANIAN <chris.chich@hotmail.fr>
2023-04-03 23:14:16 +0200

    :sparkles: Added CORS handling for both functions

--- fetch-arrival-times/index.js
+++ fetch-arrival-times/index.js
@@ -4,6 +4,22 @@ const { Firestore } = require('@google-cloud/firestore');
 const firestore = new Firestore();
 
 functions.http('fetch', async (req, res) => {
+  // Set CORS headers for preflight requests
+  // Allows GETs from any origin with the Content-Type header
+  // and caches preflight response for 3600s
+
+  res.set('Access-Control-Allow-Origin', '*');
+
+  if (req.method === 'OPTIONS') {
+    // Send response to OPTIONS requests
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Access-Control-Allow-Origin, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+    res.set('Access-Control-Max-Age', '3600');
+    res.status(204).send('');
+    return;
+  }
+
   const collection = firestore.collection('arrivalTimes');
 
   res.send({
--- push-arrival-time/index.js
+++ push-arrival-time/index.js
@@ -4,15 +4,31 @@ const { Firestore } = require('@google-cloud/firestore');
 const firestore = new Firestore();
 
 functions.http('push', async (req, res) => {
-  const collection = firestore.collection('arrivalTimes');
+    // Set CORS headers for preflight requests
+    // Allows GETs from any origin with the Content-Type header
+    // and caches preflight response for 3600s
 
-  const now = new Date();
-  const today = new Date(now.toDateString());
+    res.set('Access-Control-Allow-Origin', '*');
 
-  const document = collection.doc(today.getTime().toString());
-  document.set({
-    timestamp: now
-  }, { merge: true });
+    if (req.method === 'OPTIONS') {
+        // Send response to OPTIONS requests
+        res.setHeader("Access-Control-Allow-Credentials", "true");
+        res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+        res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Access-Control-Allow-Origin, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+        res.set('Access-Control-Max-Age', '3600');
+        res.status(204).send('');
+        return;
+    }
 
-  res.send({ success: true });
+    const collection = firestore.collection('arrivalTimes');
+
+    const now = new Date();
+    const today = new Date(now.toDateString());
+
+    const document = collection.doc(today.getTime().toString());
+    document.set({
+        timestamp: now
+    }, { merge: true });
+
+    res.send({ success: true });
 });
